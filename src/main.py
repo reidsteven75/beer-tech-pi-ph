8ddc8d4d64b3e3f6b33fbe996181126ce4ce0f10
Steven Reid <stever@veerum.com>
2019-01-06 14:43:49 -0700

    update

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from flask import Flask
+# import serial
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,4 +7,10 @@ def hello_world():
     return 'Hello World!'
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+		print('====================')
+		print('BEER TECH: PH Sensor')
+		print('====================')
+		app.run(host='0.0.0.0', port=80)
+
+		# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
+		# print(ser.name)

5dca68909cf8615b707cbcd86fb9d46cc3a28b3e
Steven Reid <stever@veerum.com>
2019-01-06 18:28:23 -0700

    update

--- src/main.py
+++ src/main.py
@@ -1,16 +1,38 @@
 from flask import Flask
-# import serial
+import serial
+import requests
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+webserver = '192.168.1.79:3000/'
+portArduinoPh = '/dev/ttyUSB0'
+portAduinoPhBaudrate = 9600
+
+def postData(rawData):
+	url = webserver + 'sensor/ph'
+	data = {'data': rawData}
+	r = requests.post(url, data)
+	print(r.text)
+
+def streamSerialUsb():
+	print("opening port {}".format(portArduinoPh))
+	ser = serial.Serial(portArduinoPh)
+	ser.baudrate = portAduinoPhBaudrate
+	
+	while True:
+		output = " "
+		print("----")
+		while output != "":
+			output = ser.readline()
+			print(output)
+			postData(output)
 
 if __name__ == '__main__':
+
 		print('====================')
 		print('BEER TECH: PH Sensor')
 		print('====================')
-		app.run(host='0.0.0.0', port=80)
 
-		# ser = serial.Serial('/dev/ttyUSB0')  # open serial port
-		# print(ser.name)
+		streamSerialUsb()
+		# app.run(host='0.0.0.0', port=80)
+
+

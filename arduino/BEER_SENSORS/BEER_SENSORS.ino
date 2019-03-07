32a5ba8d8ae4988bf2ae9fe8da61b7720a586ca1
reidsteven75 <reidsteven75@gmail.com>
2019-03-06 18:23:32 -0700

    update

--- arduino/BEER_SENSORS/BEER_SENSORS.ino
+++ arduino/BEER_SENSORS/BEER_SENSORS.ino
@@ -1,17 +1,7 @@
-/*
- # This sample code is used to test the pH meter V1.0.
- # Editor : Steve Reid
- # Ver    : 1.0
- # Product: analog pH meter
- # SKU    : SEN0169
-*/
-#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+#define PhPin A0            //pH meter Analog output to Arduino Analog Input 0
 #define Offset 0.38            //deviation compensate
-#define LED 13
-#define samplingInterval 20
-#define printInterval 800
+#define printInterval 2000
 #define ArrayLenth  40    //times of collection
-
 
 #include <SoftwareSerial.h>                           //we have to include the SoftwareSerial library, or else we can't use it
 #define rx 0                                          //define what pin rx is going to be
@@ -22,88 +12,52 @@ SoftwareSerial myserial(rx, tx);
 // PH
 int pHArray[ArrayLenth];   //Store the average value of the sensor feedback
 int pHArrayIndex=0;  
+int inputPh = 0;
 
 // TEMP
-String inputstring = "";                              //a string to hold incoming data from the PC
+String inputStringTemp = "";                              //a string to hold incoming data from the PC
+boolean inputStringTempComplete = false;
 
 // INIT
-void setup(void) {
-  pinMode(LED,OUTPUT);  
+void setup(void) { 
   Serial.begin(9600);  
-//  inputstring.reserve(10);                            //set aside some bytes for receiving data from the PC
-//  sensorstring.reserve(30);                           //set aside some bytes for receiving data from Atlas Scientific product
-//  Serial.println("sensor experiment!");    //Test the serial monitor
+  inputStringTemp.reserve(10);                            //set aside some bytes for receiving data from the PC
 }
 
 // TEMP
 void serialEvent() {                                  //if the hardware serial port_0 receives a char
-  inputstring = Serial.readStringUntil(13);           //read the string until we see a <CR>
+  while (Serial.available()) {
+    inputStringTemp = Serial.readStringUntil(13);           //read the string until we see a <CR>
+    inputStringTempComplete = true;
+  }
+ 
 }
 
-// PH
 void loop(void) {
 
   static unsigned long samplingTime = millis();
   static unsigned long printTime = millis();
   static float pHValue,voltage;
 
-  if(millis()-samplingTime > samplingInterval)
-  {
-      pHArray[pHArrayIndex++]=analogRead(SensorPin);
-      if(pHArrayIndex==ArrayLenth)pHArrayIndex=0;
-      voltage = avergearray(pHArray, ArrayLenth)*5.0/1024;
-      pHValue = 3.5*voltage+Offset;
-      samplingTime=millis();
-  }
-  if(millis() - printTime > printInterval)   //Every 800 milliseconds, print a numerical, convert the state of the LED indicator
-  {
-    Serial.print("[TEMP]");
-    Serial.println(inputstring);
+  if(millis() - printTime > printInterval)   {
+
+    // TEMP
+    if (inputStringTempComplete) {
+      Serial.print("[TEMP]");
+      Serial.println(inputStringTemp);
+      inputStringTemp = "";
+      inputStringTempComplete = false;
+    }
+
+    // PH
+    inputPh = analogRead(PhPin);
+    voltage = inputPh*5.0/1024;
+    pHValue = 3.5*voltage+Offset;
     Serial.print("[PH]");
     Serial.println(pHValue,2);
-    digitalWrite(LED,digitalRead(LED)^1);
+
+    // update print loop
     printTime=millis();
   }
 
-}
-
-// PH
-double avergearray(int* arr, int number) {
-  int i;
-  int max,min;
-  double avg;
-  long amount=0;
-  if(number<=0){
-    Serial.println("Error number for the array to avraging!/n");
-    return 0;
-  }
-  if(number<5){   //less than 5, calculated directly statistics
-    for(i=0;i<number;i++){
-      amount+=arr[i];
-    }
-    avg = amount/number;
-    return avg;
-  }else{
-    if(arr[0]<arr[1]){
-      min = arr[0];max=arr[1];
-    }
-    else{
-      min=arr[1];max=arr[0];
-    }
-    for(i=2;i<number;i++){
-      if(arr[i]<min){
-        amount+=min;        //arr<min
-        min=arr[i];
-      }else {
-        if(arr[i]>max){
-          amount+=max;    //arr>max
-          max=arr[i];
-        }else{
-          amount+=arr[i]; //min<=arr<=max
-        }
-      }//if
-    }//for
-    avg = (double)amount/(number-2);
-  }//if
-  return avg;
 }
--- src/main.py
+++ src/main.py
@@ -1,32 +1,62 @@
 from flask import Flask
 import serial
 import requests
+import threading
 from time import sleep
 app = Flask(__name__)
 
 webserver = 'https://beer-tech-web-prod.herokuapp.com'
 # webserver = 'https://beer-tech-web-qa.herokuapp.com'
-portArduinoPh = '/dev/ttyUSB0'
-portAduinoPhBaudrate = 9600
+postServerInterval = 2
+portArduino = '/dev/ttyUSB0'
+portAduinoBaudrate = 9600
 
-def postData(rawData):
-	url = webserver + '/sensor'
-	data = {'data': rawData}
-	r = requests.post(url, data)
+phTag = "[PH]"
+phString = ""
+tempTag = "[TEMP]"
+tempString = ""
 
+# posts data to server every 5 seconds
+def postData():
+
+	threading.Timer(postServerInterval, postData).start()
+
+	if (phString or tempString):
+		print('posting...')
+		print(phString)
+		print(tempString)
+		url = webserver + '/sensor'
+
+		data = {'data': phString}
+		r = requests.post(url, data)
+
+		data = {'data': tempString}
+		r = requests.post(url, data)
+
+
+# reads arduino serial data
 def streamSerialUsb():
+
+	global phString
+	global tempString
+
 	try:
-		print("opening port {}".format(portArduinoPh))
-		ser = serial.Serial(portArduinoPh)
-		ser.baudrate = portAduinoPhBaudrate
+		print("opening port {}".format(portArduino))
+		ser = serial.Serial(portArduino)
+		ser.baudrate = portAduinoBaudrate
+		ser.timeout = 1
 		
 		while True:
 			try:
 				output = " "
 				while output != "":
 					output = ser.readline()
-					print(output)
-					postData(output)
+					string = output.decode()
+					if (phTag in string):
+						phString = output
+					if (tempTag in string):
+						tempString = output
+
 			except Exception as e:
 				print("USB stream error: " + str(e))
 				break
@@ -47,6 +77,8 @@ if __name__ == '__main__':
 		print('====================')
 		print('BEER TECH: PH Sensor')
 		print('====================')
+
+		postData()
 
 		streamSerialUsb()
 		# app.run(host='0.0.0.0', port=80)
